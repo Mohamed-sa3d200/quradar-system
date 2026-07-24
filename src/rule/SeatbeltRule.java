68b5e8283aca92f61a722ce8c6ddfeab725d40b3
Mohamed Saad <mohamedsaad280705@gmail.com>
2026-07-24 07:11:06 +0300

    Rule implementation to verify driver seatbelt status

---
+++ src/rule/SeatbeltRule.java
@@ -0,0 +1,31 @@
+package rule;
+
+import model.Observation;
+import model.Violation;
+import java.util.Optional;
+
+public final class SeatbeltRule implements RadarRule {
+    private static final String RULE_NAME = "Seatbelt Fastened Rule";
+    private final double fee;
+
+    public SeatbeltRule(double fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public String getRuleName() {
+        return RULE_NAME;
+    }
+
+    @Override
+    public Optional<Violation> evaluate(Observation observation) {
+        if (!observation.isSeatbeltFastened()) {
+            return Optional.of(new Violation(
+                RULE_NAME,
+                "Seatbelt not fastned",
+                fee
+            ));
+        }
+        return Optional.empty();
+    }
+}
